5c71db8382e36386d9f7b52ee4ca5d9971aad8af
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-16 16:22:55 +0530

    added Stringsequal.java file

---
+++ StringsEqual.java
@@ -0,0 +1,13 @@
+//1.3 Write a program to check two strings are equal or not. 
+class StringsEqual{
+    public static void main(String[] args) {
+        String s1= "Internship";
+        String s2 = "Remote Job";
+        if (s1== s2){
+            System.out.println("Two Strings are equal");
+        }else{
+            System.out.println("Two Strings are not equal");
+        }
+        
+    }
+}
