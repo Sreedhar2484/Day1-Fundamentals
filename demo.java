b5e881f0e0e5d9862e31b0667a6fa58aa678bc3d
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-16 16:26:07 +0530

    added Demo.java file

---
+++ demo.java
@@ -0,0 +1,11 @@
+
+class ThisDemo {
+       int a=0;
+       void display(){
+        System.out.println(a);
+    }
+    public static void main(String args[]){
+        ThisDemo obj=new ThisDemo();
+        obj.display();
+    }
+}
