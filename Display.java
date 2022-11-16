95edaa7b31c0dd4b3d543345c0bf2ccb8c33c32b
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-16 16:24:06 +0530

    added Display.java file

---
+++ Display.java
@@ -0,0 +1,9 @@
+//1.1 Write a program to display any message:
+
+
+public class Display {
+    public static void main(String[] args) {
+      System.out.println("My name is Nagalli Sreedhar");
+    }
+  }
+
