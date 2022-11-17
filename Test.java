dd9452f13c1a9e74826ef2113ff7aa4c478f3402
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-17 11:46:56 +0530

    added Test.java file

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+class Test {
+	
+	int i;
+	double d;
+	float f;
+	boolean istrue;
+	String p;
+
+	public void printValue() {
+		System.out.println("int default value = "+ i);
+		System.out.println("double default value = "+ d);
+		System.out.println("float default value = "+ f);
+		System.out.println("boolean default value = "+ istrue);
+		System.out.println("String default value = "+ p);
+	}
+}
